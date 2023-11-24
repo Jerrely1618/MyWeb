52ff8e3e0a42b513098605172045d5a6556cafd7
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2023-11-23 21:28:59 -0500

    feat: Creating of the 404 page

--- Client/app.jsx
+++ Client/app.jsx
@@ -2,7 +2,7 @@ import HomePage from "./src/routes/home/home";
 import ComputerPage from "./src/routes/comsci";
 import LoginPage from "./src/routes/login/login";
 
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 export const appRoutes = [
   {
@@ -12,7 +12,7 @@ export const appRoutes = [
   },
 ];
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   { path: "/MyWeb/", element: <HomePage /> },
   { path: "/MyWeb/com", element: <ComputerPage /> },
   { path: "/MyWeb/logindev", element: <LoginPage /> },
--- Client/index.jsx
+++ Client/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "/public/index.css";
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider, HashRouter } from "react-router-dom"; // Import HashRouter
 import { router } from "./app.jsx";
+import HomePage from "./src/routes/home/home";
+import ComputerPage from "./src/routes/comsci";
+import LoginPage from "./src/routes/login/login";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}>
+      <HashRouter>
+        <HomePage />
+        <ComputerPage />
+        <LoginPage />
+      </HashRouter>
+    </RouterProvider>
   </React.StrictMode>
 );
