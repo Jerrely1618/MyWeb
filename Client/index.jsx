4ca3d9b245a6951b3a2f9738903b5d1c1079bbd6
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2024-04-22 22:40:06 -0400

    added speed vercel

--- Client/index.jsx
+++ Client/index.jsx
@@ -4,8 +4,10 @@ import "/public/index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app.jsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <SpeedInsights />
     <Analytics />
     <RouterProvider router={router} />
   </React.StrictMode>
